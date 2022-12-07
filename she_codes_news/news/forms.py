e5b7c4b47007694d72e4b97271e5bbbcaed3f5c0
Ms-KL <thekristyleigh@gmail.com>
2022-12-07 21:51:20 +0800

    Update forms.py
    starting to format modelform

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -1,6 +1,25 @@
-# FORMS SETUP Step 1: create forms.py and add:
-    # What are we supposed to enter for the data? 
-    # We’ll fix that using a widget in the next step.
+# -------------- NOTE ORIGINAL SETUP CODE
+# -------------- COMMENTED OUT FOR SAFETY
+
+# # FORMS SETUP Step 1: create forms.py and add:
+#     # What are we supposed to enter for the data? 
+#     # We’ll fix that using a widget in the next step.
+
+# from django import forms
+# from django.forms import ModelForm
+# from .models import NewsStory
+# class StoryForm(ModelForm):
+#     class Meta:
+#         model = NewsStory
+#         fields = ['title', 'pub_date', 'content']
+#         # USER SETUP: Step 12 Removed 'author'
+#         # FORMS SETUP Step 2: add a date picker widget
+#         widgets = {
+#             'pub_date': forms.DateInput(format=('%m/%d/%Y'),
+#             attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date',}),
+#             }
+
+# -------------- ASSIGNMENT CODE
 
 from django import forms
 from django.forms import ModelForm
@@ -11,8 +30,20 @@ class StoryForm(ModelForm):
         fields = ['title', 'pub_date', 'content']
         # USER SETUP: Step 12 Removed 'author'
         # FORMS SETUP Step 2: add a date picker widget
+        # ASSIGNMENT PART 1: STYLE THE FORM FOR ADDING NEW STORIES
+        # https://medium.com/swlh/how-to-style-your-django-forms-7e8463aae4fa
+        # https://studygyaan.com/django/django-style-the-forms-created-by-model-forms
+        # https://colinkingswood.github.io/Model-Form-Customisation/
+        # https://medium.com/swlh/how-to-style-your-django-forms-7e8463aae4fa
+        # TODO styling
         widgets = {
-            'pub_date': forms.DateInput(format=('%m/%d/%Y'),
-            attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
-            }
+            'title': forms.TextInput(attrs={
+                'class':'form-control', 
+                'style':'max-width: 300px;'}),
+            'pub_date': forms.DateInput(format=('%m/%d/%Y'), attrs={
+                'class':'form-control', 
+                'placeholder':'Select a date', 
+                'type':'date', 
+                'style':'max-width: 300px;'}),
 
+            }
