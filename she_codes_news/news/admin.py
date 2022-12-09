7f0cc00ff2b4ee62da0ddc3ab44dc6175a5c4dd4
Ms-KL <thekristyleigh@gmail.com>
2022-12-09 21:43:03 +0800

    Update admin.py
    added fields for admin page view

--- she_codes_news/news/admin.py
+++ she_codes_news/news/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+# https://docs.djangoproject.com/en/4.1/intro/tutorial07/#customize-the-admin-form
+
+from .models import NewsStory
+
+class NewsStoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Story Data',       {'fields': ['title']}),
+        (None,               {'fields': ['author']}),
+        (None,               {'fields': ['pub_date']}),
+        ('Story Content',    {'fields': ['image_field']}),
+        (None,               {'fields': ['content']}),
+    ]
+    list_display = ('title', 'pub_date')
+
+admin.site.register(NewsStory, NewsStoryAdmin)
