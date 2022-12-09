b932f001559984475d1ea983fb9d128973a67f7e
Ms-KL <thekristyleigh@gmail.com>
2022-12-09 21:46:59 +0800

    Update urls.py
    added accountview link

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,9 +1,11 @@
 # SETUP USERS Step 9: create urls for the users app (connected to createaccountview):
 
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, AccountView
 app_name = 'users'
 urlpatterns = [
     path('create-account/', CreateAccountView.as_view(),
     name='createAccount'),
+    path('view-account/', AccountView.as_view(), 
+    name='account')
 ]
