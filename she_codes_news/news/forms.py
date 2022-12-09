f4b2f43a169c7c439d67865415d11b00d6d5caf3
Ms-KL <thekristyleigh@gmail.com>
2022-12-09 12:26:02 +0800

    Update forms.py
    image field added for url

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -27,7 +27,7 @@ from .models import NewsStory
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'pub_date', 'content']
+        fields = ['title', 'pub_date', 'content', 'image_field']
         # USER SETUP: Step 12 Removed 'author'
         # FORMS SETUP Step 2: add a date picker widget
         # ASSIGNMENT PART 1: STYLE THE FORM FOR ADDING NEW STORIES
@@ -35,15 +35,21 @@ class StoryForm(ModelForm):
         # https://studygyaan.com/django/django-style-the-forms-created-by-model-forms
         # https://colinkingswood.github.io/Model-Form-Customisation/
         # https://medium.com/swlh/how-to-style-your-django-forms-7e8463aae4fa
+        # https://docs.djangoproject.com/en/4.0/topics/forms/modelforms/
+        # https://docs.djangoproject.com/en/4.0/ref/forms/fields/#imagefield
         # TODO styling
+
+        # --- added labels
+        labels = {
+            'title': ('Blog Title'),
+            'pub_date': ('Date Published'),
+            'content': ('Write your Blog'),
+            'image_field': ('Image URL'),
+        }
+
         widgets = {
-            'title': forms.TextInput(attrs={
-                'class':'form-control', 
-                'style':'max-width: 300px;'}),
             'pub_date': forms.DateInput(format=('%m/%d/%Y'), attrs={
                 'class':'form-control', 
                 'placeholder':'Select a date', 
-                'type':'date', 
-                'style':'max-width: 300px;'}),
-
+                'type':'date',}),
             }
