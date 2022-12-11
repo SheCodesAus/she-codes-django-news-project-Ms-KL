61a634e95abe5e051f227c4dcb64b7792ed4f063
Ms-KL <thekristyleigh@gmail.com>
2022-12-11 19:33:26 +0800

    views.py - added explorestories, author and reformatted
    added explore, author choice, and reformatted

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,4 +1,166 @@
-# -------------- NOTE ORIGINAL SETUP CODE
+from django.views import generic
+from django.urls import reverse_lazy #added
+from .models import NewsStory
+from .forms import StoryForm, CommentForm #added
+from django.shortcuts import render, get_object_or_404 #added
+
+
+
+
+
+class IndexView(generic.ListView):
+    template_name = 'news/index.html'
+
+    def get_queryset(self):
+        '''Return all news stories.'''
+        #  get all news stories and use in index
+        return NewsStory.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # ---- ASSIGNMENT PART 1: Order the stories by date
+        stories = NewsStory.objects.all().order_by('-pub_date')
+        context['latest_stories'] = stories[:4]
+        # display all stories from after the last "latest_story" is displayed
+        context['all_stories'] = stories[4:]
+        return context
+
+
+
+# -----------------------
+# ADD EXPLORE STORIES
+class ExploreView(generic.ListView):
+    template_name = 'news/exploreStories.html'
+
+    def get_queryset(self):
+        '''Return all news stories.'''
+        return NewsStory.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # ---- ASSIGNMENT PART 1: Order the stories by date
+        context['all_stories'] = NewsStory.objects.all().order_by('-pub_date')
+        return context
+
+
+
+# -----------------------
+    # AUTHOR CHOICE -HELP!!! (part of explore stories page)
+    # https://www.youtube.com/watch?v=xNV4GFgRCUE
+
+    def story_dropdown(request):
+        displayauthor =NewsStory.objects.values_list('author',flat=True)
+        # looks as NewsStory objects, puts the authors in a list to use
+        return render(request, 'news/exploreStories.html',{"NewsStory":displayauthor})
+        # returns a request to render displayauthor (author list from NewsStory) to explorestories.html 
+    
+    def author_view(request):
+        if request.method == 'POST':
+            selected_author = request.POST['author']
+        # Do something with the selected author...
+
+
+
+
+# -----------------------
+# SETUP NEWS Step 13: Add a view for a single story
+class StoryView(generic.DetailView):
+    model = NewsStory
+    template_name = 'news/story.html'
+    context_object_name = 'story'
+    
+    # comment - HELP!!!!
+    # def post_detail(request, slug):
+    #     post = get_object_or_404(NewsStory, slug=slug)
+    #     comments = post.comments.filter(active=True)
+    #     template_name = 'news/story.html'
+    #     new_comment = None
+    #     # Comment posted
+    #     if request.method == 'POST':
+    #         comment_form = CommentForm(data=request.POST)
+    #         if comment_form.is_valid():
+
+    #             # Create Comment object but don't save to database yet
+    #             new_comment = comment_form.save(commit=False)
+    #             # Assign the current post to the comment
+    #             new_comment.post = post
+    #             # Save the comment to the database
+    #             new_comment.save()
+    #     else:
+    #         comment_form = CommentForm()
+
+    #     return render(request, template_name, {'NewsStory': NewsStory,
+    #                                         'comments': comments,
+    #                                         'new_comment': new_comment,
+    #                                         'comment_form': comment_form})
+
+# FORMS SETUP Step 1: add a view to use the form
+
+
+
+
+# -----------------------
+class AddStoryView(generic.CreateView):
+    form_class = StoryForm
+    context_object_name = 'storyForm'
+    template_name = 'news/createStory.html'
+    success_url = reverse_lazy('news:index')
+
+    # USERS SETUP Step 12:  update the view to complete the author field
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# -----------------------
+
+# add comments
+# def post_detail(request, slug):
+#     template_name = 'post_detail.html'
+#     post = get_object_or_404(NewsStory, slug=slug)
+#     comments = post.comments.filter(active=True)
+#     new_comment = None
+#     # Comment posted
+#     if request.method == 'POST':
+#         comment_form = CommentForm(data=request.POST)
+#         if comment_form.is_valid():
+
+#             # Create Comment object but don't save to database yet
+#             new_comment = comment_form.save(commit=False)
+#             # Assign the current post to the comment
+#             new_comment.post = post
+#             # Save the comment to the database
+#             new_comment.save()
+#     else:
+#         comment_form = CommentForm()
+
+#     return render(request, template_name, {'NewsStory': NewsStory,
+#                                            'comments': comments,
+#                                            'new_comment': new_comment,
+#                                            'comment_form': comment_form})
+
+# -----------------------
+
+# ---- ASSIGNMENT PART 1: Order the stories by date
+            # add ordering to models - refer to this in views
+            # https://medium.com/@thexovc/how-to-order-blogpost-in-django-53218db7b092
+            # doesn't work:
+            # return NewsStory.objects.all.order_by('-pub_date')
+        # https://docs.djangoproject.com/en/4.0/intro/tutorial03/
+        # latest_story_list = NewsStory.objects.order_by('-pub_date')
+        # context = {'latest_story_list': latest_story_list}
+        # return render(self,'news/index.html', context)
+        # # KG NOTE this uses import render and import NewsStory
+
+# NOTE
+# referred to urls.py
+# class based view (part 4 in tutorial)
+    # can use function based view
+    # similar, but looks different
+
+
+# -----------------------
+    # -------------- NOTE ORIGINAL SETUP CODE
 # -------------- COMMENTED OUT FOR SAFETY
 
 # # FORMS SETUP Step 1: add a view to use the form
@@ -44,115 +206,3 @@
 # # class based view (part 4 in tutorial)
 #     # can use function based view
 #     # similar, but looks different
-
-# -------------- ASSIGNMENT CODE -------------
-
-# FORMS SETUP Step 1: add a view to use the form
-from django.views import generic
-from django.urls import reverse_lazy #added
-from .models import NewsStory
-from .forms import StoryForm, CommentForm #added
-from django.shortcuts import render, get_object_or_404 #added
-
-class IndexView(generic.ListView):
-    template_name = 'news/index.html'
-
-    def get_queryset(self):
-        '''Return all news stories.'''
-        #  get all news stories and use in index
-        return NewsStory.objects.all()
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # ---- ASSIGNMENT PART 1: Order the stories by date
-        stories = NewsStory.objects.all().order_by('-pub_date')
-        context['latest_stories'] = stories[:4]
-        # display all stories from after the last "latest_story" is displayed
-        context['all_stories'] = stories[4:]
-        return context
-
-# SETUP NEWS Step 13: Add a view for a single story
-class StoryView(generic.DetailView):
-    model = NewsStory
-    template_name = 'news/story.html'
-    context_object_name = 'story'
-    
-    # comment - HELP!!!!
-    # def post_detail(request, slug):
-    #     post = get_object_or_404(NewsStory, slug=slug)
-    #     comments = post.comments.filter(active=True)
-    #     template_name = 'news/story.html'
-    #     new_comment = None
-    #     # Comment posted
-    #     if request.method == 'POST':
-    #         comment_form = CommentForm(data=request.POST)
-    #         if comment_form.is_valid():
-
-    #             # Create Comment object but don't save to database yet
-    #             new_comment = comment_form.save(commit=False)
-    #             # Assign the current post to the comment
-    #             new_comment.post = post
-    #             # Save the comment to the database
-    #             new_comment.save()
-    #     else:
-    #         comment_form = CommentForm()
-
-    #     return render(request, template_name, {'NewsStory': NewsStory,
-    #                                         'comments': comments,
-    #                                         'new_comment': new_comment,
-    #                                         'comment_form': comment_form})
-
-# FORMS SETUP Step 1: add a view to use the form
-class AddStoryView(generic.CreateView):
-    form_class = StoryForm
-    context_object_name = 'storyForm'
-    template_name = 'news/createStory.html'
-    success_url = reverse_lazy('news:index')
-
-    # USERS SETUP Step 12:  update the view to complete the author field
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-
-# add comments
-# def post_detail(request, slug):
-#     template_name = 'post_detail.html'
-#     post = get_object_or_404(NewsStory, slug=slug)
-#     comments = post.comments.filter(active=True)
-#     new_comment = None
-#     # Comment posted
-#     if request.method == 'POST':
-#         comment_form = CommentForm(data=request.POST)
-#         if comment_form.is_valid():
-
-#             # Create Comment object but don't save to database yet
-#             new_comment = comment_form.save(commit=False)
-#             # Assign the current post to the comment
-#             new_comment.post = post
-#             # Save the comment to the database
-#             new_comment.save()
-#     else:
-#         comment_form = CommentForm()
-
-#     return render(request, template_name, {'NewsStory': NewsStory,
-#                                            'comments': comments,
-#                                            'new_comment': new_comment,
-#                                            'comment_form': comment_form})
-
-# ---- ASSIGNMENT PART 1: Order the stories by date
-            # add ordering to models - refer to this in views
-            # https://medium.com/@thexovc/how-to-order-blogpost-in-django-53218db7b092
-            # doesn't work:
-            # return NewsStory.objects.all.order_by('-pub_date')
-        # https://docs.djangoproject.com/en/4.0/intro/tutorial03/
-        # latest_story_list = NewsStory.objects.order_by('-pub_date')
-        # context = {'latest_story_list': latest_story_list}
-        # return render(self,'news/index.html', context)
-        # # KG NOTE this uses import render and import NewsStory
-
-# NOTE
-# referred to urls.py
-# class based view (part 4 in tutorial)
-    # can use function based view
-    # similar, but looks different
