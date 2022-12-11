18d611a2455a0ccca57aa4d75ec858e7912b5f9c
Ms-KL <thekristyleigh@gmail.com>
2022-12-11 22:54:31 +0800

    formatting of models

--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -1,11 +1,14 @@
 # SETUP USERS Step 2: Create the new user model
 
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 class CustomUser(AbstractUser):
+    # profile_image = models.URLField(blank=True)
     # KG: Users comes with Django
     # KG: Write custom user class, use the inbuilt User function and abstract to inherit the user functions and build on it
-    pass
+    # pass
 
     def __str__(self):
         return self.username
