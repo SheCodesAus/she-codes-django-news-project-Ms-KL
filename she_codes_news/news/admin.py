f15e4846226d6f8bd0abd3ed9c966196f5e59396
Ms-KL <thekristyleigh@gmail.com>
2022-12-11 11:00:18 +0800

    Update admin.py - to include comment function

--- she_codes_news/news/admin.py
+++ she_codes_news/news/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 # https://docs.djangoproject.com/en/4.1/intro/tutorial07/#customize-the-admin-form
 
-from .models import NewsStory
+from .models import NewsStory, Comment
 
 class NewsStoryAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -16,3 +16,15 @@ class NewsStoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date')
 
 admin.site.register(NewsStory, NewsStoryAdmin)
+
+# COMMENT
+# https://djangocentral.com/creating-comments-system-with-django/
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('commenter_name', 'comment_body', 'comment_post', 'comment_created_on', 'comment_active')
+    list_filter = ('comment_active', 'comment_created_on')
+    search_fields = ('commenter_name', 'comment_email', 'comment_body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
