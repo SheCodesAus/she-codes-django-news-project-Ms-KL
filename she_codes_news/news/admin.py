52269af2527d80d7289f6ff4587586ef913e79d4
Ms-KL <thekristyleigh@gmail.com>
2022-12-12 15:11:05 +0800

    admin.py - added commentadmin
    with Ben's guidance

--- she_codes_news/news/admin.py
+++ she_codes_news/news/admin.py
@@ -13,18 +13,39 @@ class NewsStoryAdmin(admin.ModelAdmin):
         ('Story Content',    {'fields': ['image_field']}),
         (None,               {'fields': ['content']}),
     ]
-    list_display = ('title', 'pub_date')
+    list_display = ('title','author', 'pub_date')
 
 admin.site.register(NewsStory, NewsStoryAdmin)
 
-# COMMENT HELP
-# https://djangocentral.com/creating-comments-system-with-django/
+
+# COMMENT BEN
+# https://youtu.be/SImJVdZocvQ
+# from . import models
+# admin.site.register(models.Comment)
+
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('commenter_name', 'comment_body', 'comment_post', 'comment_created_on', 'comment_active')
-    list_filter = ('comment_active', 'comment_created_on')
-    search_fields = ('commenter_name', 'comment_email', 'comment_body')
-    actions = ['approve_comments']
+    list_display = ('story', 'author', 'content', 'created_at', 'modified_at')
+    list_filter = ('created_at','story')
+    search_fields = ('author', 'content','story')
 
-    def approve_comments(self, request, queryset):
-        queryset.update(active=True)
+
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('story', 'author', 'content', 'created_at', 'modified_at')
+#     list_filter = ('created_at')
+#     search_fields = ('author', 'content')
+
+
+# COMMENT HELP
+# https://djangocentral.com/creating-comments-system-with-django/
+
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('commenter_name', 'comment_body', 'comment_post', 'comment_created_on', 'comment_active')
+#     list_filter = ('comment_active', 'comment_created_on')
+#     search_fields = ('commenter_name', 'comment_email', 'comment_body')
+#     actions = ['approve_comments']
+
+#     def approve_comments(self, request, queryset):
+#         queryset.update(active=True)
