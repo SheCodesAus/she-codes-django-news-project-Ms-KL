9815b059e4e8f29dc0944bbf2628fe6a9d8a7578
Ms-KL <thekristyleigh@gmail.com>
2022-12-12 15:12:40 +0800

    models.py - simplified comment form based on Ben's guidance

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,15 +1,14 @@
 from django.contrib.auth import get_user_model #ADDED
 from django.db import models
 
-
+USER = get_user_model()
 
 
 # -----------------------
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
-        get_user_model(),
-        on_delete=models.CASCADE
+        USER, on_delete=models.CASCADE
     )
     pub_date = models.DateTimeField()
     content = models.TextField()
@@ -20,26 +19,26 @@ class NewsStory(models.Model):
     def __str__(self):
         return self.title
 
-
-
-
-
 # -----------------------
-# comment - HELP!!!!
-# https://djangocentral.com/creating-comments-system-with-django/
+# COMMENT
+# https://youtu.be/SImJVdZocvQ
 class Comment(models.Model):
-    comment_post = models.ForeignKey(NewsStory,on_delete=models.CASCADE,related_name='comments')
-    commenter_name = models.CharField(max_length=80)
-    comment_email = models.EmailField()
-    comment_body = models.TextField()
-    comment_created_on = models.DateTimeField(auto_now_add=True)
-    comment_active = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ['comment_created_on']
+    story = models.ForeignKey(NewsStory,on_delete=models.CASCADE,related_name='comments')
+    author = models.ForeignKey(
+        USER, related_name="comments", on_delete=models.CASCADE, null=True,
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return 'Comment {} by {}'.format(self.comment_body, self.commenter_name)
+        return self.story
+
+    # class Meta:
+    #     ordering = ['comment_created_on']
+
+    # def __str__(self):
+    #     return 'Comment {} by {}'.format(self.comment_body, self.commenter_name)
 
 
 
