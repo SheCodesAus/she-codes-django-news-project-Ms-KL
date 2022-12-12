5485164b0fe94152830fdd1ea4c696cd036e9feb
Ms-KL <thekristyleigh@gmail.com>
2022-12-12 15:14:36 +0800

    urls.py - added comment url

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -5,11 +5,14 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+
     path('explore-all-stories/', views.ExploreView.as_view(), name='allStories'),
-    # NEWS SETUP Step 13: Use the view (for single story) in the URLS
+
     path('<int:pk>/', views.StoryView.as_view(), name='story'),
-    # FORMS SETUP Step 1: add form to the urls
-    # What are we supposed to enter for the data? 
-    # We’ll fix that using a widget in the next step.
+
+    path('<int:pk>/comment/', views.AddCommentView.as_view(), name="addComment"),
+
     path('add-story/', views.AddStoryView.as_view(), name='newStory'),
+
+
 ]
